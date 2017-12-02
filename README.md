5139f3163a7c2bd4b0af29f6e4bae7469b68f259
Faustino Vasquez Limon <fvasquez@fvasquez>
2017-12-01 17:41:13 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# curso-vue-fvasquez
+
+> Aplicacion para el curso vue 2 de fvasquez
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/TaskForm.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h2 class="subtitle">Nueva Tarea:</h2>
+
+        <form @submit.prevent="create" class="new-task-form">
+            <input v-model="draft" type="text" class="form-control">
+            <button class="btn btn-primary">Crear Tarea</button>
+        </form>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+          return{
+              draft:''
+          }
+        },
+        methods: {
+            create() {
+                this.$emit('created',{
+                    description: this.draft,
+                    pending: true,
+                    editing: false,
+                });
+                this.draft = '';
+            }
+        }
+    }
+</script>
+
+<style>
+    .new-task-form {
+        display: flex;
+    }
+    .new-task-form input {
+        margin-right: 10px;
+    }
+</style>
